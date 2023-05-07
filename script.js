ac0263d5198c807a9882aa99ffc9a7ff31ab1e25
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-05-07 13:19:20 +0200

    create selectors

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const userFirstNum = Number(prompt("first num"));
 const userSecondNum = Number(prompt("second num"));
 const userOperator = prompt("operator");
 
+const mathOperations = document.querySelector(".math-operations");
+const inputField = document.querySelector(".input-field");
+const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".backspace");
+const percent = document.querySelector(".percent");
+const division = document.querySelector(".division");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const times = document.querySelector(".times");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const minus = document.querySelector(".minus");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector("three");
+const plus = document.querySelector(".plus");
+const plusMinus = document.querySelector(".plus-minus");
+const zero = document.querySelector(".zero");
+const dot = document.querySelector(".dot");
+const equals = document.querySelector(".equals");
+
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
 }
