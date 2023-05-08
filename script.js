b341ec31d481d8bc3b2fce6b1bae2d4ff698fd99
adamnorred <123160854+adamnorred@users.noreply.github.com>
2023-05-08 14:15:35 +0200

    create part of display function

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const userFirstNum = Number(prompt("first num"));
-const userSecondNum = Number(prompt("second num"));
-const userOperator = prompt("operator");
+// const userFirstNum = Number(prompt("first num"));
+// const userSecondNum = Number(prompt("second num"));
+// const userOperator = prompt("operator");
 
 const mathOperations = document.querySelector(".math-operations");
 const inputField = document.querySelector(".input-field");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector("three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
 const clear = document.querySelector(".clear");
 const backspace = document.querySelector(".backspace");
 const percent = document.querySelector(".percent");
 const division = document.querySelector(".division");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector(".nine");
 const times = document.querySelector(".times");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
 const minus = document.querySelector(".minus");
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector("three");
 const plus = document.querySelector(".plus");
 const plusMinus = document.querySelector(".plus-minus");
 const zero = document.querySelector(".zero");
@@ -55,3 +55,41 @@ function operator(firstNum, secondNum, operator) {
     return divide(firstNum, secondNum);
   }
 }
+
+one.addEventListener("click", function () {
+  inputField.textContent += this.textContent;
+  mathOperations.textContent += this.textContent;
+});
+
+two.addEventListener("click", function () {
+  inputField.textContent += this.textContent;
+  mathOperations.textContent += this.textContent;
+});
+
+plus.addEventListener("click", function () {
+  if (
+    mathOperations.textContent.includes(plus.textContent) ||
+    mathOperations.textContent.includes(minus.textContent) ||
+    mathOperations.textContent.includes(times.textContent) ||
+    mathOperations.textContent.includes(division.textContent)
+  ) {
+    mathOperations.textContent =
+      mathOperations.textContent.slice(0, -1) + `${this.textContent}`;
+  } else {
+    mathOperations.textContent += ` ${this.textContent}`;
+  }
+});
+
+minus.addEventListener("click", function () {
+  if (
+    mathOperations.textContent.includes(plus.textContent) ||
+    mathOperations.textContent.includes(minus.textContent) ||
+    mathOperations.textContent.includes(times.textContent) ||
+    mathOperations.textContent.includes(division.textContent)
+  ) {
+    mathOperations.textContent =
+      mathOperations.textContent.slice(0, -1) + `${this.textContent}`;
+  } else {
+    mathOperations.textContent += ` ${this.textContent}`;
+  }
+});
